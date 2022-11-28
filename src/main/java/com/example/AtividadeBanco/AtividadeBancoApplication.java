afc070b63224007b305afe15c658643f077f130e
Caique do Vale <caiquedovale_@hotmail.com>
2022-11-28 19:44:34 -0300

    AtividadeBanco

---
+++ src/main/java/com/example/AtividadeBanco/AtividadeBancoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AtividadeBanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeBancoApplication.class, args);
+	}
+
+}
